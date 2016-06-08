82b4261550a037e76729d180dd413b40fda7333b
iury <satriani-16@hotmail.com>
2016-06-08 13:06:36 -0300

    pegando as configuracoes de variaveis de ambiente

--- src/settings.py
+++ src/settings.py
@@ -3,8 +3,8 @@
 import os
 
 
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-DEBUG = True
+MONGO_HOST = os.getenv("MONGO_HOST", 'localhost')
+MONGO_PORT = os.getenv("MONGO_PORT", 27017)
+DEBUG = os.getenv("DEBUG", False)
 ENABLE_CORS = True
 SECRET_KEY = os.getenv("SECRET_KEY")
